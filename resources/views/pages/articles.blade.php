45d7af22c96a56ecdf7f7ef26c6043c72ea2d181
Скриплюк В <v.skriplyuk@qsoft.ru>
2022-10-20 15:23:05 +0000

    228582 / Раздел управления новостями

--- resources/views/pages/articles.blade.php
+++ resources/views/pages/articles.blade.php
@@ -5,6 +5,10 @@
 
 @section('inner-content')
             <div class="space-y-4">
+                
+                <x-addArticleForm.result.result/>
+
+                <a class="text-orange" href="{{ route('articles.create') }}"><span class="text-sm text-white italic rounded bg-black px-2">Создать новость</span></a>
 
                 <x-articlesPage.allArticles :allArticles="$allArticles"/>
             
